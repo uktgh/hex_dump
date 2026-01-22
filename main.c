679af25bbf048e75c79e22bb83abb21b74a8e539
uktgh <159624128+uktgh@users.noreply.github.com>
2026-01-22 16:38:15 +0100

    v01

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_clear(char* s, int len) {
+    printf("|");
+
+    for (int i = 0; i < len; i++) {
+        if (s[i] <= 0x20 || s[i] >= 0x7E) {
+            // resplace special character with .
+            printf(".");
+        } else {
+            // print the actual character
+            printf("%c", s[i]);
+        }
+    }
+
+    printf("|");
+}
+
+int main () {
+    int c;
+    int mem_i = 0;
+    char* s = (char*) calloc(0x10, sizeof(char));
+
+    while ((c = getchar()) != EOF) {
+        s[mem_i % 0x10] = c;
+
+        if (mem_i % 0x10 == 0x0) {
+            printf("%08x ", mem_i);
+        }
+
+        printf("%02x ", c);
+        mem_i++;
+
+        if (mem_i % 0x10 == 0x0) {
+            // last memory address reached
+            print_clear(s, 0x10);
+            printf("\n");
+        }
+    }
+
+    printf("%*s", 3*(0x10 - (mem_i % 0x10)), " ");
+    print_clear(s, (mem_i % 0x10));
+    printf("\n%08x", mem_i);
+    printf("\n");
+
+    return 0;
+}
