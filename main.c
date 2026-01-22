6075420af4b042a23d52bdc0486640bd09ba0d94
uktgh <159624128+uktgh@users.noreply.github.com>
2026-01-22 16:45:04 +0100

    Fix formatting issues in main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ void print_clear(char* s, int len) {
 
     for (int i = 0; i < len; i++) {
         if (s[i] <= 0x20 || s[i] >= 0x7E) {
-            // resplace special character with .
+            // replace special character with .
             printf(".");
         } else {
             // print the actual character
@@ -46,3 +46,4 @@ int main () {
 
     return 0;
 }
+
